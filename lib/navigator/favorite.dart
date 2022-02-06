026cec466f78b25af39e16c5b58f1cfbdbd6ffaf
flutterfahimrajbari <flutterfahimrajbari@gmail.com>
2022-02-06 15:56:01 +0600

    first commit

---
+++ lib/navigator/favorite.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Favoritewidget extends StatefulWidget {
+  const Favoritewidget({ Key? key }) : super(key: key);
+
+  @override
+  _favoritewidgetState createState() => _favoritewidgetState();
+}
+
+class _favoritewidgetState extends State<Favoritewidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
